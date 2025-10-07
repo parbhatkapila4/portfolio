85a0f3b7c00779a1627a273fcad19c2ed54bf798
parbhatkapila4 <p4parbhatk4kapila@gmail.com>
2025-10-07 17:37:56 +0530

    Last Changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -35,7 +35,7 @@ const Header = () => {
                 Parbhat Kapila
               </h1>
               <p className="text-muted-foreground text-sm sm:text-base font-medium leading-none">
-               AI Full-Stack Engineer
+                Full-Stack Engineer
                 <br />
                 <span className="font-medium text-xs sm:text-sm">
                   AI • Backend Systems
@@ -54,7 +54,7 @@ const Header = () => {
                       Parbhat Kapila
                     </h1>
                     <p className="text-muted-foreground text-lg  leading-none font-medium">
-                     AI Full-Stack Engineer
+                      Full-Stack Engineer
                       <br />
                       <span className="text-base font-medium">
                         AI • Backend Systems
